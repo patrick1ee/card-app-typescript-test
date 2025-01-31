644bde1b043b4be1f9e470dad1dcf12dc3663266
Patrick Lee <patricklee927@gmail.com>
2025-01-31 10:00:43 +0000

    backend: tests pass

--- backend/test/server.test.ts
+++ backend/test/server.test.ts
@@ -1,27 +1,42 @@
 import Prisma from "../src/db";
 import { server } from "../src/server";
 
+
+/* Setup/tear-down*/
+
+beforeAll(async () => {
+  await Prisma.entry.deleteMany()
+})
+
+afterEach(async () => {
+  await Prisma.entry.deleteMany();
+});
+
+
+/* Fetching entries*/
+
 describe("GET /get/", () => {
   it("should assert that the fetched entries array is empty", async () => {
-    Prisma.entry.findMany = jest.fn().mockResolvedValue([]);
     const response = await server.inject({
       method: "GET",
       url: "/get/",
     });
-
     expect(response.statusCode).toBe(200);
-    expect(JSON.parse(response.body)).toEqual([]);
+
+    const data = response.json()
+    expect(data).toHaveLength(0);
   });
 });
 
 describe("GET /get/", () => {
   it("should return all entries in the database", async () => {
     const mockEntries = [
-      { id: "1", title: "Test Entry 1", created_at: new Date(), scheduled: new Date("2025-02-02T00:00:00") },
-      { id: "2", title: "Test Entry 2", created_at: new Date(), scheduled: new Date("2025-03-02T09:00:00") },
+      { id: "1", title: "Test Entry 1", description: "Test Description 1", created_at: new Date(), scheduled: new Date("2025-02-02T00:00:00.000Z") },
+      { id: "2", title: "Test Entry 2", description: "Test Description 2", created_at: new Date("2025-04-01T09:00:00.000Z"), scheduled: new Date("2025-04-02T09:00:00.000Z") },
     ];
 
-    Prisma.entry.findMany = jest.fn().mockResolvedValue(mockEntries);
+    await Prisma.entry.create({data: mockEntries[0]})
+    await Prisma.entry.create({data: mockEntries[1]})
 
     const response = await server.inject({
       method: "GET",
@@ -35,7 +50,10 @@ describe("GET /get/", () => {
     }));
 
     expect(response.statusCode).toBe(200);
-    expect(JSON.parse(response.body)).toEqual(expectedEntries);
+    
+    const data = response.json()
+    expect(data).toHaveLength(2)
+    expect(data.sort((a: any, b: any) => a.id - b.id)).toEqual(expectedEntries);
   });
 });
 
@@ -44,11 +62,12 @@ describe("GET /get/:id", () => {
     const mockEntry = {
       id: "1",
       title: "Test Entry",
+      description: "Test Description",
       created_at: new Date("2025-01-29T23:39:32.021Z"),
       scheduled: new Date("2025-02-02T00:00:00.000Z"),
     };
 
-    Prisma.entry.findUnique = jest.fn().mockResolvedValue(mockEntry);
+    await Prisma.entry.create({data: mockEntry})
 
     const response = await server.inject({
       method: "GET",
@@ -62,11 +81,12 @@ describe("GET /get/:id", () => {
     };
 
     expect(response.statusCode).toBe(200);
-    expect(JSON.parse(response.body)).toEqual(expectedEntry);
+    
+    const data = response.json()
+    expect(data).toEqual(expectedEntry);
   });
 
   it("should return a 500 error if the entry is not found", async () => {
-    Prisma.entry.findUnique = jest.fn().mockResolvedValue(null);
 
     const response = await server.inject({
       method: "GET",
@@ -74,51 +94,66 @@ describe("GET /get/:id", () => {
     });
 
     expect(response.statusCode).toBe(500);
-    expect(JSON.parse(response.body)).toEqual({
+
+    const data = response.json()
+    expect(data).toEqual({
       msg: "Error finding entry with id nonexistent",
     });
   });
 });
+
+
+/* Creating entries */
 
 describe("POST /create/", () => {
   it("should create a new entry", async () => {
     const newEntry = {
       id: "1",
       title: "New Entry",
-      created_at: new Date("2025-01-29T23:39:32.021Z"),
-      scheduled: new Date("2025-02-02T00:00:00.000Z"),
+      description: "New Description",
+      created_at: "2025-01-29T23:39:32.000Z",
+      scheduled: "2025-02-02T00:00:00.000Z",
     };
-
-    Prisma.entry.create = jest.fn().mockResolvedValue(newEntry);
-
     const response = await server.inject({
       method: "POST",
       url: "/create/",
-      payload: {
-        id: "1",
-        title: "New Entry",
-        created_at: "2025-01-29T23:39:32.021Z",
-        scheduled: "2025-02-02T00:00:00.000Z",
-      },
+      payload: newEntry
     });
 
-    const expectedEntry = {
-      ...newEntry,
-      created_at: newEntry.created_at.toISOString(),
-      scheduled: newEntry.scheduled.toISOString(),
-    };
-
     expect(response.statusCode).toBe(200);
-    expect(JSON.parse(response.body)).toEqual(expectedEntry);
+
+    const data = response.json()
+    expect(data).toEqual(newEntry);
   });
 
-  it("should return a 500 error if creation fails", async () => {
-    Prisma.entry.create = jest.fn().mockRejectedValue(new Error("Database error"));
+  it("should return a 500 error if entry input missing values", async () => {
 
-    const response = await server.inject({
+  const response = await server.inject({
       method: "POST",
       url: "/create/",
       payload: { id: "1", title: "Failed Entry" },
+    });
+
+    expect(response.statusCode).toBe(500);
+
+    const data = response.json()
+    expect(data).toEqual({ msg: "Error creating entry" });
+  });
+
+  it("should return a 500 error if entry input contains extraneous values", async () => {
+
+    const newEntry = {
+      id: "1",
+      title: "New Entry",
+      descripion: "New Description",
+      created_at: new Date("2025-01-29T23:39:32.021Z"),
+      scheduled: new Date("2025-02-02T00:00:00.000Z"),
+      extra: "extra"
+    };
+    const response = await server.inject({
+      method: "POST",
+      url: "/create/",
+      payload: newEntry
     });
 
     expect(response.statusCode).toBe(500);
@@ -126,67 +161,88 @@ describe("POST /create/", () => {
   });
 });
 
+
+/* Deleting entries */
+
 describe("DELETE /delete/:id", () => {
   it("should delete an entry by ID", async () => {
-    Prisma.entry.delete = jest.fn().mockResolvedValue({});
+    const mockEntry = {
+      id: "1",
+      title: "Test Entry",
+      description: "Test Description",
+      created_at: new Date("2025-01-29T23:39:32.021Z"),
+      scheduled: new Date("2025-02-02T00:00:00.000Z"),
+    };
 
-    const response = await server.inject({
+    await Prisma.entry.create({data: mockEntry})
+
+    let response = await server.inject({
       method: "DELETE",
       url: "/delete/1",
     });
 
     expect(response.statusCode).toBe(200);
-    expect(JSON.parse(response.body)).toEqual({ msg: "Deleted successfully" });
+
+    let data = response.json()
+    expect(data).toEqual({ msg: "Deleted successfully" });
+
+    response = await server.inject({
+      method: "GET",
+      url: "/get/",
+    });
+    data = response.json()
+    expect(data).toHaveLength(0)
   });
 
-  it("should return a 500 error if deletion fails", async () => {
-    Prisma.entry.delete = jest.fn().mockRejectedValue(new Error("Database error"));
-
+  it("should return a 500 error if entry does not exist", async () => {
     const response = await server.inject({
       method: "DELETE",
       url: "/delete/1",
     });
 
     expect(response.statusCode).toBe(500);
-    expect(JSON.parse(response.body)).toEqual({ msg: "Error deleting entry" });
+
+    const data = response.json()
+    expect(data).toEqual({ msg: "Error deleting entry" });
   });
 });
 
+
+/* Updating entries */
+
 describe("PUT /update/:id", () => {
   it("should update an entry by ID", async () => {
-    const updatedEntry = {
+    const mockEntry = {
       id: "1",
-      title: "Updated Entry",
+      title: "Test Entry",
+      description: "Test Description",
       created_at: new Date("2025-01-29T23:39:32.021Z"),
       scheduled: new Date("2025-02-02T00:00:00.000Z"),
     };
 
-    Prisma.entry.update = jest.fn().mockResolvedValue(updatedEntry);
+    await Prisma.entry.create({data: mockEntry})
+
+    const updatedEntry = {
+      id: "1",
+      title: "Updated Entry",
+      description: "Updated Description",
+      created_at: new Date("2025-01-29T23:39:32.021Z"),
+      scheduled: new Date("2025-02-02T00:00:00.000Z"),
+    };
 
     const response = await server.inject({
       method: "PUT",
       url: "/update/1",
-      payload: {
-        id: "1",
-        title: "Updated Entry",
-        created_at: "2025-01-29T23:39:32.021Z",
-        scheduled: "2025-02-02T00:00:00.000Z",
-      },
+      payload: updatedEntry
     });
 
-    const expectedEntry = {
-      ...updatedEntry,
-      created_at: updatedEntry.created_at.toISOString(),
-      scheduled: updatedEntry.scheduled.toISOString(),
-    };
-
     expect(response.statusCode).toBe(200);
-    expect(JSON.parse(response.body)).toEqual({ msg: "Updated successfully" });
+
+    const data = response.json()
+    expect(data).toEqual({ msg: "Updated successfully" });
   });
 
   it("should return a 500 error if update fails", async () => {
-    Prisma.entry.update = jest.fn().mockRejectedValue(new Error("Database error"));
-
     const response = await server.inject({
       method: "PUT",
       url: "/update/1",
@@ -194,6 +250,8 @@ describe("PUT /update/:id", () => {
     });
 
     expect(response.statusCode).toBe(500);
-    expect(JSON.parse(response.body)).toEqual({ msg: "Error updating" });
+
+    const data = response.json()
+    expect(data).toEqual({ msg: "Error updating" });
   });
 });
